bf00958583eacc0a36dfa87fe27801fdfc7e4ffd
Paulo Gabriel <pgsacarvalho@gmail.com>
2020-08-20 02:05:23 -0300

    Created migrations with tables and rellationships

---
+++ src/shared/infra/typeorm/migrations/1597898822135-AddCustomerToOrders.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+import { QueryExpressionMap } from 'typeorm/query-builder/QueryExpressionMap';
+
+export default class AddCustomerToOrders1597898822135
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customer_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomer',
+        columnNames: ['customer_id'],
+        referencedTableName: 'customers',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
+}
